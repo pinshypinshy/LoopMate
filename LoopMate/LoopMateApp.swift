15a60b1ff14cbf47b8066132e9a69d52ee3bba96
pinshypinshy <hrisyklit@gmail.com>
2026-03-03 12:25:07 +0900

    Initial Commit

---
+++ LoopMate/LoopMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoopMateApp.swift
+//  LoopMate
+//
+//  Created by 平石悠生 on 2026/03/03.
+//
+
+import SwiftUI
+
+@main
+struct LoopMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
